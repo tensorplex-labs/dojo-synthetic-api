dcf85ed70c943a80d57f8db98278f7fc6e986327
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-05-08 18:33:24 +0800

    refactor: remove bittensor from code

--- commons/utils.py
+++ commons/utils.py
@@ -1,10 +1,6 @@
-import argparse
 import asyncio
 import socket
 import urllib.parse
-from typing import Tuple
-
-import bittensor as bt
 
 
 def build_url(ip_addr, port):
@@ -28,34 +24,3 @@ async def is_live(host: str, port: int = 80, timeout=2):
     else:
         sock.close()
         return True
-
-
-def get_config():
-    """Returns the configuration object specific to this miner or validator after adding relevant arguments."""
-    parser = argparse.ArgumentParser()
-    bt.subtensor.add_args(parser)
-    bt.logging.add_args(parser)
-    _config = bt.config(parser)
-    bt.logging.check_config(_config)
-    return _config
-
-
-def initialise(
-    config: bt.config,
-) -> Tuple[bt.subtensor, bt.metagraph]:
-    # These are core Bittensor classes to interact with the network.
-    bt.logging.info("Setting up bittensor objects....")
-    # The subtensor is our connection to the Bittensor blockchain.
-    subtensor = bt.subtensor(config=config)
-    bt.logging.info(f"Subtensor: {subtensor}")
-    # The metagraph holds the state of the network, letting us know about other validators and miners.
-    metagraph = subtensor.metagraph(config.netuid)
-    bt.logging.info(f"Metagraph: {metagraph}")
-    # The axon handles request processing, allowing validators to send this miner requests.
-    return subtensor, metagraph
-
-
-def is_miner(metagraph: bt.metagraph, uid: int) -> bool:
-    """Check if uid is a validator. NOTE: this code is duplicated on subnet code side."""
-    stakes = metagraph.S.tolist()
-    return stakes[uid] < 1_000
--- main.py
+++ main.py
@@ -6,6 +6,62 @@ from fastapi.middleware.cors import CORSMiddleware
 from commons.routes.code_gen import code_gen_router
 from commons.routes.synthetic_gen import synthetic_gen_router
 
+import time
+from ipaddress import ip_address, ip_network
+
+import httpx
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+MAX_CONTENT_LENGTH = 1 * 1024 * 1024
+
+
+class IPFilterMiddleware(BaseHTTPMiddleware):
+    """Middleware to ensure that only requests from AWS Servers are allowed."""
+
+    _aws_ips_url = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+    _allowed_ip_ranges = []
+    _last_checked: float = 0
+    _allowed_networks = []
+
+    @classmethod
+    async def _get_allowed_networks(cls):
+        return [ip_network(ip_range) for ip_range in await cls._get_allowed_ip_ranges()]
+
+    # TODO
+    # @classmethod
+    # async def _get_allowed_ip_ranges(cls):
+    #     if (time.time() - cls._last_checked) < 300:
+    #         return cls._allowed_ip_ranges
+
+    #     async with httpx.AsyncClient() as client:
+    #         start_time = time.time()
+    #         response = await client.get(cls._aws_ips_url)
+    #         cls._last_checked = time.time()
+    #         data = response.json()
+    #         cls._allowed_ip_ranges = [
+    #             ip_range["ip_prefix"]
+    #             for ip_range in data["prefixes"]
+    #             if ip_range["region"] in [US_EAST_REGION]
+    #         ]
+    #     return cls._allowed_ip_ranges
+
+    def __init__(self, app):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next):
+        client_ip = ip_address(request.client.host)
+        allowed_networks = [
+            ip_network(ip_range) for ip_range in await self._get_allowed_networks()
+        ]
+        for network in allowed_networks:
+            if client_ip in network:
+                response = await call_next(request)
+                return response
+        return Response("Forbidden", status_code=403)
+
+
 app = FastAPI()
 
 # Add CORS middleware to allow cross-origin requests
@@ -15,6 +71,7 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
 
 # Include the code_gen router
 app.include_router(code_gen_router)
