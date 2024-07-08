50e4aae0e041f3e81a71ee7351d379658b5d321b
neuralninjathesecond <neuralninja@tensorplex.ai>
2024-07-08 10:31:29 +0800

    added test for execution error

--- commons/tests/test_executor.py
+++ commons/tests/test_executor.py
@@ -1,6 +1,7 @@
 import sys
 
 sys.path.append("./")
+from commons.utils.utils import ExecutionError
 from commons.utils.python_executor import PythonExecutor
 
 
@@ -16,3 +17,17 @@ fig.write_html("test.html")
     executor.main()
 
     assert "test.html" in executor.created_files.keys()
+    
+def test_execution_error():
+    test_code = """
+import plotly.express as px
+df = px.data.iris()
+df["e"] = df["sepal_width"]/100
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", error_x="e", error_y="e")
+fig.show()
+"""  
+    executor = PythonExecutor(code=test_code)
+    try:
+        executor.main()
+    except Exception as e:
+        assert isinstance(e, ExecutionError)
