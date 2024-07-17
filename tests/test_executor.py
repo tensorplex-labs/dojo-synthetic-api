c8453a0a56930036b1e80aa92b47a4de17659a47
neuralninjathesecond <neuralninja@tensorplex.ai>
2024-07-17 16:43:35 +0800

    chore: add test for plotly regex

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -32,3 +32,60 @@ fig.show()
         executor.main()
     except Exception as e:
         assert isinstance(e, ExecutionError)
+        
+def test_plotly_regex():
+    test_code = ["""
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.write_html("fig.html", include_plotlyjs='cdn')
+""",
+"""
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.write_html("fig.html")
+""",
+"""
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.write_html("fig.html", include_plotlyjs=False)
+"""]
+    
+    expected_code = """
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.write_html("fig.html", include_plotlyjs='cdn')
+"""
+    for code in test_code:
+        assert PythonExecutor.modify_plotly_to_html(code) == expected_code
