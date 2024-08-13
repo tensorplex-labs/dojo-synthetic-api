3086f7af1941c983974c66497f50e34817bd1963
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-08-13 22:23:12 +0800

    fix: temporarily remove non-leaderboard model

--- commons/dataset/__init__.py
+++ commons/dataset/__init__.py
@@ -41,7 +41,7 @@ ANSWER_MODELS = [
     "openai/gpt-4-turbo-2024-04-09",
     "openai/gpt-4-1106-preview",
     "openai/gpt-3.5-turbo-1106",
-    "openai/gpt-4o",
+    # "openai/gpt-4o",
     "anthropic/claude-3-opus-20240229",
     "anthropic/claude-3-sonnet-20240229",
     "anthropic/claude-3-haiku-20240307",
